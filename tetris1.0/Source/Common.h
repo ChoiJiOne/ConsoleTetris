cd868635d1e067c8607d4ddc39fe9b6e0d45b4d3
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-03-17 21:39:13 +0900

    Common.h 헤더 선언 추가
    - 메모리 누수 추적을 위한 crtdbg.h 추가

---
+++ tetris1.0/Source/Common.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define _CRTDBG_MAP_ALLOC
+
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <chrono>
+#include <random>
+#include <locale>
+#include <codecvt>
+#include <crtdbg.h>
+#include <Windows.h>
+
+#undef SetConsoleTitle
