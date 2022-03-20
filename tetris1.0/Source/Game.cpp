301ad99e7b23eeb7cb0ef643f9fccc2bd08b3a88
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-03-21 07:46:49 +0900

    테트리스 로직 구현

--- tetris1.0/Source/Game.cpp
+++ tetris1.0/Source/Game.cpp
@@ -11,65 +11,261 @@ Game::Game()
 
 Game::~Game()
 {
+	nextTetromino.reset();
+	currTetromino.reset();
+	tetrisBoard.reset();
 }
 
 void Game::Setup()
 {
-	ConsoleUtil::SetConsoleWindow(100, 100, 500, 400);
 	ConsoleUtil::SetConsoleTitle("tetris1.0");
 	ConsoleUtil::SetConsoleCursorVisible(false);
+
+	uiPositionCache["tetrisBoard"]   = Vector2i(10, 0);
+	uiPositionCache["nextTetromino"] = Vector2i(22, 2);
+	uiPositionCache["remainingTime"] = Vector2i(22, 8);
+	uiPositionCache["level"]         = Vector2i(22, 9);
+	uiPositionCache["keyArrow"]      = Vector2i(24, 12);
+	
+	tetrisBoard = std::make_shared<Board>();
+	currTetromino = TetrominoGenerator::GenerateRandomTetromino(Vector2i(tetrisBoard->GetColSize() / 3, 0));
+	nextTetromino = TetrominoGenerator::GenerateRandomTetromino(Vector2i(tetrisBoard->GetColSize() / 3, 0));
+
+	AddTetrominoInBoard(*currTetromino, *tetrisBoard);
 }
 
 void Game::Run()
 {
-	Tetromino tetromino(Vector2i(4, 0), Tetromino::EShape::L);
-	Board board;
-
 	while (!bIsDoneGame)
 	{
-		if (GetAsyncKeyState(VK_ESCAPE) & 0x8000)
-		{
-			bIsDoneGame = true;
-		}
+		Update();
+		Draw();
+	}
 
-		if (GetAsyncKeyState(VK_UP) & 0x8000)
+	ConsoleUtil::ClearConsole();
+	ConsoleUtil::ShowTextInConsole(0, 0, "thank you for your play", EConsoleTextColor::BrightWhite);
+}
+
+void Game::Update()
+{
+	if (GetAsyncKeyState(VK_ESCAPE) & 0x8000)
+	{
+		bIsDoneGame = true;
+	}
+
+	if (GetAsyncKeyState(VK_UP) & 0x8000)
+	{
+		if (SpinClockWiseTetrominoInBoard(*currTetromino, *tetrisBoard))
 		{
-			tetromino.Spin(Tetromino::ESpin::CW);
 			bIsDraw = true;
-		}
-
-		if (bIsDraw)
-		{
-			ConsoleUtil::ClearConsole();
-
-			Vector2i absolPos = tetromino.GetAbsolutePosition();
-			const std::vector<Vector2i> positions = tetromino.GetRelativePositions();
-
-			for (const auto& position : positions)
-			{
-				ConsoleUtil::ShowTextInConsole(absolPos.x + position.x, absolPos.y + position.y, "■", EConsoleTextColor::Blue);
-			}
-
-			Vector2i boardPos = Vector2i(0, 0);
-			int32_t rowSize = board.GetRowSize();
-			int32_t colSize = board.GetColSize();
-
-			for (int32_t y = 0; y < rowSize; ++y)
-			{
-				for (int32_t x = 0; x < colSize; ++x)
-				{
-					Board::EBlockState state = board.GetBoardBlockState(x, y);
-
-					if (state == Board::EBlockState::Fill || state == Board::EBlockState::Fix)
-					{
-						ConsoleUtil::ShowTextInConsole(x + boardPos.x, y + boardPos.y, "■", EConsoleTextColor::Green);
-					}
-				}
-			}
-
-			bIsDraw = false;
 		}
 	}
 
-	ConsoleUtil::SetConsoleTextColor(EConsoleTextColor::BrightWhite);
+	if (GetAsyncKeyState(VK_DOWN) & 0x8000)
+	{
+		if (MoveDownTetrominoInBoard(*currTetromino, *tetrisBoard))
+		{
+			bIsDraw = true;
+		}
+	}
+
+	if (GetAsyncKeyState(VK_LEFT) & 0x8000)
+	{
+		if (MoveLeftTetrominoInBoard(*currTetromino, *tetrisBoard))
+		{
+			bIsDraw = true;
+		}
+	}
+
+	if (GetAsyncKeyState(VK_RIGHT) & 0x8001)
+	{
+		if (MoveRightTetrominoInBoard(*currTetromino, *tetrisBoard))
+		{
+			bIsDraw = true;
+		}
+	}
+}
+
+void Game::Draw()
+{
+	if (bIsDraw)
+	{
+		ConsoleUtil::ClearConsole();
+
+		Vector2i boardPos = uiPositionCache["tetrisBoard"];
+		int32_t rowSize = tetrisBoard->GetRowSize();
+		int32_t colSize = tetrisBoard->GetColSize();
+
+		for (int32_t y = 0; y < rowSize; ++y)
+		{
+			for (int32_t x = 0; x < colSize; ++x)
+			{
+				Board::EBlockState state = tetrisBoard->GetBoardBlockState(x, y);
+
+				if (state == Board::EBlockState::Fix)
+				{
+					ConsoleUtil::ShowTextInConsole(x + boardPos.x, y + boardPos.y, "■", EConsoleTextColor::Green);
+				}
+				else if (state == Board::EBlockState::Fill)
+				{
+					ConsoleUtil::ShowTextInConsole(x + boardPos.x, y + boardPos.y, "■", EConsoleTextColor::Blue);
+				}
+			}
+		}
+
+		Vector2i nextTetrominoPos = uiPositionCache["nextTetromino"];
+		ConsoleUtil::ShowTextInConsole(nextTetrominoPos.x, nextTetrominoPos.y, "다음 테트로미노", EConsoleTextColor::Green);
+		const std::vector<Vector2i>& positions = nextTetromino->GetRelativePositions();
+
+		for (const auto& position : positions)
+		{
+			Vector2i consolePos = nextTetrominoPos + position;
+			ConsoleUtil::ShowTextInConsole(consolePos.x + 1, consolePos.y + 1, "■", EConsoleTextColor::Blue);
+			ConsoleUtil::ShowTextInConsole(consolePos.x + 1, consolePos.y + 1, "■", EConsoleTextColor::Blue);
+		}
+
+		Vector2i remainingTimePos = uiPositionCache["remainingTime"];
+		ConsoleUtil::ShowTextInConsole(remainingTimePos.x, remainingTimePos.y, "남은 시간 : s", EConsoleTextColor::Green);
+
+		Vector2i levelPos = uiPositionCache["level"];
+		ConsoleUtil::ShowTextInConsole(levelPos.x, levelPos.y, "현재 레벨 : ", EConsoleTextColor::Green);
+
+		Vector2i keyArrowPos = uiPositionCache["keyArrow"];
+		ConsoleUtil::ShowTextInConsole(keyArrowPos.x, keyArrowPos.y, "■", EConsoleTextColor::Green);
+		ConsoleUtil::ShowTextInConsole(keyArrowPos.x - 1, keyArrowPos.y, "←", EConsoleTextColor::Green);
+		ConsoleUtil::ShowTextInConsole(keyArrowPos.x + 1, keyArrowPos.y, "→", EConsoleTextColor::Green);
+		ConsoleUtil::ShowTextInConsole(keyArrowPos.x, keyArrowPos.y - 1, "↑", EConsoleTextColor::Green);
+		ConsoleUtil::ShowTextInConsole(keyArrowPos.x, keyArrowPos.y + 1, "↓", EConsoleTextColor::Green);
+
+		bIsDraw = false;
+	}
+}
+
+void Game::AddTetrominoInBoard(Tetromino& tetromino, Board& board)
+{
+	const std::vector<Vector2i> positions = tetromino.GetRelativePositions();
+
+	for (const auto& position : positions)
+	{
+		Vector2i absolPos = tetromino.GetAbsolutePosition() + position;
+		board.SetBoardBlockState(absolPos.x, absolPos.y, Board::EBlockState::Fill);
+	}
+}
+
+void Game::RemoveTetrominoInBoard(Tetromino& tetromino, Board& board)
+{
+	const std::vector<Vector2i> positions = tetromino.GetRelativePositions();
+
+	for (const auto& position : positions)
+	{
+		Vector2i boardPos = tetromino.GetAbsolutePosition() + position;
+		board.SetBoardBlockState(boardPos.x, boardPos.y, Board::EBlockState::Empty);
+	}
+}
+
+bool Game::IsCrashTetrominoAndBoard(Tetromino& tetromino, Board& board)
+{
+	const std::vector<Vector2i> positions = tetromino.GetRelativePositions();
+
+	for (const auto& position : positions)
+	{
+		Vector2i boardPos = tetromino.GetAbsolutePosition() + position;
+
+		if (!board.CheckInsidePositionInBoard(boardPos.x, boardPos.y))
+		{
+			return true;
+		}
+		
+		if (board.GetBoardBlockState(boardPos.x, boardPos.y) != Board::EBlockState::Empty)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool Game::MoveDownTetrominoInBoard(Tetromino& tetromino, Board& board)
+{
+	bool bCanMove = true;
+	RemoveTetrominoInBoard(tetromino, board);
+	
+	tetromino.Move(Tetromino::EMove::Down);
+
+	if (IsCrashTetrominoAndBoard(tetromino, board))
+	{
+		tetromino.Move(Tetromino::EMove::Up);
+		bCanMove = false;
+	}
+
+	AddTetrominoInBoard(tetromino, board);
+	return bCanMove;
+}
+
+bool Game::MoveLeftTetrominoInBoard(Tetromino& tetromino, Board& board)
+{
+	bool bCanMove = true;
+	RemoveTetrominoInBoard(tetromino, board);
+
+	tetromino.Move(Tetromino::EMove::Left);
+
+	if (IsCrashTetrominoAndBoard(tetromino, board))
+	{
+		tetromino.Move(Tetromino::EMove::Right);
+		bCanMove = false;
+	}
+
+	AddTetrominoInBoard(tetromino, board);
+	return bCanMove;
+}
+
+bool Game::MoveRightTetrominoInBoard(Tetromino& tetromino, Board& board)
+{
+	bool bCanMove = true;
+	RemoveTetrominoInBoard(tetromino, board);
+
+	tetromino.Move(Tetromino::EMove::Right);
+
+	if (IsCrashTetrominoAndBoard(tetromino, board))
+	{
+		tetromino.Move(Tetromino::EMove::Left);
+		bCanMove = false;
+	}
+
+	AddTetrominoInBoard(tetromino, board);
+	return bCanMove;
+}
+
+bool Game::SpinClockWiseTetrominoInBoard(Tetromino& tetromino, Board& board)
+{
+	bool bCanMove = true;
+	RemoveTetrominoInBoard(tetromino, board);
+
+	tetromino.Spin(Tetromino::ESpin::CW);
+
+	if (IsCrashTetrominoAndBoard(tetromino, board))
+	{
+		tetromino.Spin(Tetromino::ESpin::CCW);
+		bCanMove = false;
+	}
+
+	AddTetrominoInBoard(tetromino, board);
+	return bCanMove;
+}
+
+bool Game::SpinCounterClockWiseTetrominoInBoard(Tetromino& tetromino, Board& board)
+{
+	bool bCanMove = true;
+	RemoveTetrominoInBoard(tetromino, board);
+
+	tetromino.Spin(Tetromino::ESpin::CCW);
+
+	if (IsCrashTetrominoAndBoard(tetromino, board))
+	{
+		tetromino.Spin(Tetromino::ESpin::CW);
+		bCanMove = false;
+	}
+
+	AddTetrominoInBoard(tetromino, board);
+	return bCanMove;
 }
--- tetris1.0/Source/Game.h
+++ tetris1.0/Source/Game.h
@@ -2,6 +2,10 @@
 
 #include "Common.h"
 
+
+class Tetromino;
+class Board;
+
 class Game
 {
 public:
@@ -11,7 +15,29 @@ public:
 	void Setup();
 	void Run();
 
+	void Update();
+	void Draw();
+
+private:
+	void AddTetrominoInBoard(Tetromino& tetromino, Board& board);
+	void RemoveTetrominoInBoard(Tetromino& tetromino, Board& board);
+	bool IsCrashTetrominoAndBoard(Tetromino& tetromino, Board& board);
+
+	bool MoveDownTetrominoInBoard(Tetromino& tetromino, Board& board);
+	bool MoveLeftTetrominoInBoard(Tetromino& tetromino, Board& board);
+	bool MoveRightTetrominoInBoard(Tetromino& tetromino, Board& board);
+
+	bool SpinClockWiseTetrominoInBoard(Tetromino& tetromino, Board& board);
+	bool SpinCounterClockWiseTetrominoInBoard(Tetromino& tetromino, Board& board);
+
+
 private:
 	bool bIsDoneGame = false;
 	bool bIsDraw     = true;
+
+	std::shared_ptr<Board>     tetrisBoard   = nullptr;
+	std::shared_ptr<Tetromino> currTetromino = nullptr;
+	std::shared_ptr<Tetromino> nextTetromino = nullptr;
+
+	std::unordered_map<std::string, struct Vector2i> uiPositionCache;
 };
