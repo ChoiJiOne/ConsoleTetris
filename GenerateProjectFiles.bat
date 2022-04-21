b4bc1535beb3b71117e31a6824f640b1a3c757b5
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-04-21 23:07:51 +0900

    프로젝트 생성 스크립트 수정
    - CMake 대신 premake5를 사용하도록 수정
    - 프로젝트 생성 스크립트의 실행 결과로 생기는 결과물을 커밋할 수 없도록 .gitignore 파일 수정

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -1,14 +1,13 @@
 @echo off
 
-echo Generate Project Files...
+echo Generate Project Files ...
 
 if exist tetris1.0 (
-	echo remove legacy folder
+	echo Remove Legacy Folder
 	rmdir /s /q tetris1.0
 )
 
 mkdir tetris1.0
-pushd tetris1.0
-cmake ..
+premake5.exe vs2019
 popd tetris1.0
 PAUSE
