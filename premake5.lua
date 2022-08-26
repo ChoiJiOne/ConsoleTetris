b4e2848d0e5c5e994c1dd481ad96aeda194c03bc
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-08-26 18:47:07 +0900

    premake5.lua 수정
    - 빌드 구성 요소 추가
    - C++ 17 표준 기능을 사용하도록 변경

--- premake5.lua
+++ premake5.lua
@@ -1,24 +1,66 @@
+-- ConsoleTetris 작업 영역입니다.
 workspace "ConsoleTetris"
 
-   configurations { "Debug", "Release" }
-   platforms { "Win64" }
-   location "ConsoleTetris"
+   -- 빌드 구성 요소입니다.
+   configurations { 
+      "Debug",
+      "Release",
+      "Development",
+      "Shipping",
+   }
 
+   -- 빌드 플랫폼입니다.
+   platforms { "Win64" }
+
+   -- 솔루션 경로를 설정합니다.
+   location "%{wks.name}"
+
+   -- 소스 코드 경로를 설정합니다.
+   sourcePath="%{wks.location}/../Source"
+
+   -- ConsoleTetris 프로젝트입니다.
    project "ConsoleTetris"
+
+      -- ConsoleTetris 프로젝트의 종류를 설정합니다.
       kind "ConsoleApp"
+
+      -- 프로그래밍 언어를 설정합니다.
       language "C++"
       
-      files { 
-         "%{wks.location}/../Source/*.cpp",
-         "%{wks.location}/../Source/*.h"
+      -- C++의 표준을 설정합니다.
+      cppdialect "C++17"
+
+      -- 소스 코드의 include 경로를 추가합니다.
+      includedirs {
+         "%{sourcePath}"
       }
 
+      -- 소스 코드의 file을 추가합니다.
+      files { 
+         "%{sourcePath}/*",
+      }
+
+      -- 빌드의 구성 요소별 설정을 수행합니다.
       filter "configurations:Debug"
          defines { "DEBUG" }
+         runtime  "Debug"
+         optimize "Off"
          symbols "On"
 
       filter "configurations:Release"
          defines { "NDEBUG" }
+         runtime "Release"
          optimize "On"
+         symbols "On"
 
+      filter "configurations:Development"
+            defines { "NDEBUG" }
+            runtime "Release"
+            optimize "On"
+            symbols "On"
 
+      filter "configurations:Shipping"
+            defines { "NDEBUG" }
+            runtime "Release"
+            optimize "Full"
+            symbols "Off"
