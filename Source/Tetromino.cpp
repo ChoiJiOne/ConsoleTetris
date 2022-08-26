ccbe134bbf476ae114592ae4736b8e9c6584e172
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2022-08-27 08:47:12 +0900

    Tetromino class 선언 및 구현

---
+++ Source/Tetromino.cpp
@@ -0,0 +1,238 @@
+#include "Macro.h"
+#include "Tetromino.h"
+
+Tetromino::Tetromino(const Math::Vec2i& InAbsolutionPosition, const EType& InType, const Block::EColor& InBlockColor)
+	: TetrominoBlock(Block(InBlockColor, Block::EState::Fill))
+	, AbsolutePosition(InAbsolutionPosition)
+{
+	CreateRelativePositions(InType, RelativePositions, AreaSize, BoundSize);
+}
+
+Tetromino::Tetromino(Tetromino&& InInstance) noexcept
+	: TetrominoBlock(InInstance.TetrominoBlock)
+	, BoundSize(InInstance.BoundSize)
+	, AreaSize(InInstance.AreaSize)
+	, AbsolutePosition(InInstance.AbsolutePosition)
+	, RelativePositions(InInstance.RelativePositions)
+{
+}
+
+Tetromino::Tetromino(const Tetromino& InInstance) noexcept
+	: TetrominoBlock(InInstance.TetrominoBlock)
+	, BoundSize(InInstance.BoundSize)
+	, AreaSize(InInstance.AreaSize)
+	, AbsolutePosition(InInstance.AbsolutePosition)
+	, RelativePositions(InInstance.RelativePositions)
+{
+}
+
+Tetromino& Tetromino::operator=(Tetromino&& InInstance) noexcept
+{
+	if (this == &InInstance) return *this;
+
+	TetrominoBlock = InInstance.TetrominoBlock;
+	BoundSize = InInstance.BoundSize;
+	AreaSize = InInstance.AreaSize;
+	AbsolutePosition = InInstance.AbsolutePosition;
+	RelativePositions = InInstance.RelativePositions;
+
+	return *this;
+}
+
+Tetromino& Tetromino::operator=(const Tetromino& InInstance) noexcept
+{
+	if (this == &InInstance) return *this;
+
+	TetrominoBlock = InInstance.TetrominoBlock;
+	BoundSize = InInstance.BoundSize;
+	AreaSize = InInstance.AreaSize;
+	AbsolutePosition = InInstance.AbsolutePosition;
+	RelativePositions = InInstance.RelativePositions;
+
+	return *this;
+}
+
+Tetromino Tetromino::CreateRandomTetromino(const Math::Vec2i& InAbsolutionPosition)
+{
+	static EType Types[] = {
+		EType::I,
+		EType::O,
+		EType::T,
+		EType::J,
+		EType::L,
+		EType::S,
+		EType::Z
+	};
+
+	static Block::EColor Colors[] = {
+		Block::EColor::Blue,
+		Block::EColor::Green,
+		Block::EColor::Aqua,
+		Block::EColor::Red,
+		Block::EColor::Purple,
+		Block::EColor::Yellow,
+		Block::EColor::White,
+	};
+
+	int32_t TypeIndex  = Math::GenerateRandomInt<int32_t>(0, std::size(Types) - 1);
+	int32_t ColorIndex = Math::GenerateRandomInt<int32_t>(0, std::size(Colors) - 1);
+
+	return Tetromino(InAbsolutionPosition, Types[TypeIndex], Colors[ColorIndex]);
+}
+
+Tetromino::~Tetromino()
+{
+}
+
+Tetromino::EMovement Tetromino::GetCountMovement(const EMovement& InMovement)
+{
+	EMovement CountMovement;
+
+	switch (InMovement)
+	{
+	case EMovement::CW:
+		CountMovement = EMovement::CCW;
+		break;
+
+	case EMovement::CCW:
+		CountMovement = EMovement::CW;
+		break;
+
+	case EMovement::Down:
+		CountMovement = EMovement::Up;
+		break;
+
+	case EMovement::Up:
+		CountMovement = EMovement::Down;
+		break;
+
+	case EMovement::Left:
+		CountMovement = EMovement::Right;
+		break;
+
+	case EMovement::Right:
+		CountMovement = EMovement::Left;
+		break;
+
+	default:
+		ENFORCE_THROW_EXCEPTION("undefined movement type");
+	}
+
+	return CountMovement;
+}
+
+void Tetromino::Move(const EMovement& InMovement)
+{
+	switch (InMovement)
+	{
+	case EMovement::Up:
+		AbsolutePosition.y -= 1;
+		break;
+
+	case EMovement::Down:
+		AbsolutePosition.y += 1;
+		break;
+
+	case EMovement::Left:
+		AbsolutePosition.x -= 1;
+		break;
+
+	case EMovement::Right:
+		AbsolutePosition.x += 1;
+		break;
+
+	case EMovement::CCW:
+		RelativePositions[0] = Math::Vec2i(RelativePositions[0].y, BoundSize - 1 - RelativePositions[0].x);
+		RelativePositions[1] = Math::Vec2i(RelativePositions[1].y, BoundSize - 1 - RelativePositions[1].x);
+		RelativePositions[2] = Math::Vec2i(RelativePositions[2].y, BoundSize - 1 - RelativePositions[2].x);
+		RelativePositions[3] = Math::Vec2i(RelativePositions[3].y, BoundSize - 1 - RelativePositions[3].x);
+		break;
+
+	case EMovement::CW:
+		RelativePositions[0] = Math::Vec2i(BoundSize - 1 - RelativePositions[0].y, RelativePositions[0].x);
+		RelativePositions[1] = Math::Vec2i(BoundSize - 1 - RelativePositions[1].y, RelativePositions[1].x);
+		RelativePositions[2] = Math::Vec2i(BoundSize - 1 - RelativePositions[2].y, RelativePositions[2].x);
+		RelativePositions[3] = Math::Vec2i(BoundSize - 1 - RelativePositions[3].y, RelativePositions[3].x);
+		break;
+
+	default:
+		ENFORCE_THROW_EXCEPTION("undefined movement type");
+	}
+}
+
+void Tetromino::Draw(const Math::Vec2i& InPosition)
+{
+	for (const auto& RelativePosition : RelativePositions)
+	{
+		Math::Vec2i CurrentPosition = InPosition + RelativePosition;
+		TetrominoBlock.Draw(CurrentPosition);
+	}
+}
+
+void Tetromino::CreateRelativePositions(const EType& InType, std::vector<Math::Vec2i>& OutRelativePositions, int32_t& OutAreaSize, int32_t& OutBoundSize)
+{
+	OutRelativePositions.resize(4);
+	OutAreaSize = 4;
+
+	switch (InType)
+	{
+	case EType::I:
+		OutBoundSize = 4;
+		OutRelativePositions[0] = Math::Vec2i(0, 1);
+		OutRelativePositions[1] = Math::Vec2i(1, 1);
+		OutRelativePositions[2] = Math::Vec2i(2, 1);
+		OutRelativePositions[3] = Math::Vec2i(3, 1);
+		break;
+
+	case EType::O:
+		OutBoundSize = 4;
+		OutRelativePositions[0] = Math::Vec2i(1, 1);
+		OutRelativePositions[1] = Math::Vec2i(2, 1);
+		OutRelativePositions[2] = Math::Vec2i(1, 2);
+		OutRelativePositions[3] = Math::Vec2i(2, 2);
+		break;
+
+	case EType::T:
+		OutBoundSize = 3;
+		OutRelativePositions[0] = Math::Vec2i(0, 1);
+		OutRelativePositions[1] = Math::Vec2i(1, 1);
+		OutRelativePositions[2] = Math::Vec2i(2, 1);
+		OutRelativePositions[3] = Math::Vec2i(1, 2);
+		break;
+
+	case EType::J:
+		OutBoundSize = 3;
+		OutRelativePositions[0] = Math::Vec2i(0, 1);
+		OutRelativePositions[1] = Math::Vec2i(1, 1);
+		OutRelativePositions[2] = Math::Vec2i(2, 1);
+		OutRelativePositions[3] = Math::Vec2i(2, 2);
+		break;
+
+	case EType::L:
+		OutBoundSize = 3;
+		OutRelativePositions[0] = Math::Vec2i(0, 1);
+		OutRelativePositions[1] = Math::Vec2i(1, 1);
+		OutRelativePositions[2] = Math::Vec2i(2, 1);
+		OutRelativePositions[3] = Math::Vec2i(0, 2);
+		break;
+
+	case EType::S:
+		OutBoundSize = 3;
+		OutRelativePositions[0] = Math::Vec2i(1, 0);
+		OutRelativePositions[1] = Math::Vec2i(2, 0);
+		OutRelativePositions[2] = Math::Vec2i(0, 1);
+		OutRelativePositions[3] = Math::Vec2i(1, 1);
+		break;
+
+	case EType::Z:
+		OutBoundSize = 3;
+		OutRelativePositions[0] = Math::Vec2i(0, 0);
+		OutRelativePositions[1] = Math::Vec2i(0, 1);
+		OutRelativePositions[2] = Math::Vec2i(1, 1);
+		OutRelativePositions[3] = Math::Vec2i(2, 1);
+		break;
+
+	default:
+		ENFORCE_THROW_EXCEPTION("undefined tetromino type");
+	}
+}
---
+++ Source/Tetromino.h
@@ -0,0 +1,200 @@
+#pragma once
+
+#include "Block.h"
+
+
+/**
+ * 테트리스의 테트로미노 클래스입니다.
+ */
+class Tetromino
+{
+public:
+	/**
+	 * 테트로미노의 타입입니다.
+	 *
+	 * @see https://ko.wikipedia.org/wiki/%ED%85%8C%ED%8A%B8%EB%A1%9C%EB%AF%B8%EB%85%B8
+	 */
+	enum class EType : int32_t
+	{
+		I = 0,
+		O = 1,
+		T = 2,
+		J = 3,
+		L = 4,
+		S = 5,
+		Z = 6
+	};
+
+
+	/**
+	 * 테트로미노의 움직임입니다.
+	 */
+	enum class EMovement : int32_t
+	{
+		Left = 0,
+		Right = 1,
+		Down = 2,
+		Up = 3,
+		CCW = 4,
+		CW = 5
+	};
+
+
+public:
+	/**
+	 * 테트로미노 클래스 생성자입니다.
+	 *
+	 * @param InAbsolutionPosition - 테트리스 보드 상의 테트로미노 왼쪽 상단 위치입니다.
+	 * @param InType - 테트로미노의 타입입니다.
+	 * @param InBlockColor - 테트로미노 블럭의 색깔입니다.
+	 */
+	Tetromino(const Math::Vec2i& InAbsolutionPosition, const EType& InType, const Block::EColor& InBlockColor);
+
+
+	/**
+	 * 테트로미노 클래스의 복사 생성자입니다.
+	 *
+	 * @param InInstance - 복사를 수행할 보드 객체입니다.
+	 */
+	Tetromino(Tetromino&& InInstance) noexcept;
+
+
+	/**
+	 * 테트로미노 클래스의 복사 생성자입니다.
+	 *
+	 * @param InInstance - 복사를 수행할 보드 객체입니다.
+	 */
+	Tetromino(const Tetromino& InInstance) noexcept;
+
+
+	/**
+	 * 테트로미노 클래스의 대입 연산자입니다.
+	 *
+	 * @param InInstance - 복사를 수행할 보드 객체입니다.
+	 *
+	 * @return 대입을 수행한 객체의 참조자를 반환합니다.
+	 */
+	Tetromino& operator=(Tetromino&& InInstance) noexcept;
+
+
+	/**
+	 * 테트로미노 클래스의 대입 연산자입니다.
+	 *
+	 * @param InInstance - 복사를 수행할 보드 객체입니다.
+	 *
+	 * @return 대입을 수행한 객체의 참조자를 반환합니다.
+	 */
+	Tetromino& operator=(const Tetromino& InInstance) noexcept;
+
+
+	/**
+	 * 테트로미노를 랜덤으로 생성합니다.
+	 *
+	 * @param InAbsolutionPosition - 테트리스 보드 상의 테트로미노 왼쪽 상단 위치입니다.
+	 *
+	 * @return 렌덤으로 생성된 테트로미노를 반환합니다.
+	 */
+	static Tetromino CreateRandomTetromino(const Math::Vec2i& InAbsolutionPosition);
+
+
+	/**
+	 * 테트리스의 테트로미노 클래스의 소멸자입니다.
+	 */
+	virtual ~Tetromino();
+
+
+	/**
+	 * 테트로미노의 블럭을 얻습니다.
+	 *
+	 * @return 테트로미노의 블럭을 반환합니다.
+	 */
+	Block GetTetrominoBlock() const { return TetrominoBlock; }
+
+
+	/**
+	 * 테트리스 보드 상의 테트로미노 왼쪽 상단 위치를 얻습니다.
+	 *
+	 * @return 테트리스 보드 상의 테트로미노 왼쪽 상단 위치를 반환합니다.
+	 */
+	Math::Vec2i GetAbsolutePosition() const { return AbsolutePosition; }
+
+
+	/**
+	 * 테트로미노 블럭의 상대적인 위치를 얻습니다.
+	 *
+	 * @return 테트로미노 블럭의 상대적인 위치를 반환합니다.
+	 */
+	const std::vector<Math::Vec2i>& GetRelativePositions() const { return RelativePositions; }
+
+
+	/**
+	 * 테트로미노 움직임의 반대 방향을 얻습니다.
+	 *
+	 * @param InMovement - 반대 방향을 알고 싶은 움직임입니다.
+	 *
+	 * @return 움직임의 반대 방향을 반환합니다.
+	 */
+	static EMovement GetCountMovement(const EMovement& InMovement);
+
+
+	/**
+	 * 테트로미노를 움직입니다.
+	 *
+	 * @param InMovement - 테트로미노의 움직임 종류 입니다.
+	 */
+	void Move(const EMovement& InMovement);
+
+
+	/**
+	 * 테트로미노를 콘솔 화면에 그립니다.
+	 *
+	 * @param InCenterPosition - 콘솔 화면상의 테트로미노 왼쪽 상단의 좌표입니다.
+	 *
+	 * @throws 콘솔 화면에 그리기를 실패하면 C++ 표준 예외를 던집니다.
+	 */
+	void Draw(const Math::Vec2i& InPosition);
+
+
+private:
+	/**
+	 * 타입에 따른 테트로미노 블럭의 상대적인 위치를 생성합니다.
+	 *
+	 * @param InType - 테트로미노의 타입입니다.
+	 * @param OutRelativePositions - 테트로미노 블럭의 상대적인 위치입니다.
+	 * @param OutAreaSize - 테트로미노의 영역 크기입니다.
+	 * @param OutBoundSize - 테트로미노의 크기입니다.
+	 */
+	static void CreateRelativePositions(const EType& InType, std::vector<Math::Vec2i>& OutRelativePositions, int32_t& OutAreaSize, int32_t& OutBoundSize);
+
+
+private:
+	/**
+	 * 테트로미노의 블럭입니다.
+	 */
+	Block TetrominoBlock;
+
+
+	/**
+	 * 테트로미노의 크기입니다.
+	 * 이때, 테트로미노의 영역은 정사각형입니다.
+	 */
+	int32_t BoundSize = 0;
+
+
+	/**
+	 * 테트로미노의 영역 크기입니다.
+	 */
+	int32_t AreaSize = 0;
+
+
+	/**
+	 * 테트리스 보드 상의 테트로미노 왼쪽 상단 위치입니다.
+	 */
+	Math::Vec2i AbsolutePosition;
+
+
+	/**
+	 * 테트로미노 블럭의 상대적인 위치입니다.
+	 */
+	std::vector<Math::Vec2i> RelativePositions;
+};
