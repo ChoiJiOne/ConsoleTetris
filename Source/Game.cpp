81cc64da5e5e7fc3b358f99d3d9cba265a427495
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2022-08-27 10:46:11 +0900

    Game class 루프 구현

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -1,13 +1,126 @@
+#include "Macro.h"
 #include "Game.h"
 
 Game::~Game()
 {
+	for (auto& TargetTetromino : Tetrominos)
+	{
+		TargetTetromino.reset();
+	}
+
+	TetrisBoard.reset();
 }
 
 void Game::Init()
 {
+	Console::Clear();
+	Console::SetTitle("ConsoleTetris");
+	Console::SetCursorVisible(false);
+
+	StartPosition = Math::Vec2i(3, 0);
+
+	for (int32_t CountOfTetromino = 1; CountOfTetromino <= 6; ++CountOfTetromino)
+	{
+		std::unique_ptr<Tetromino> NewTetromino = std::make_unique<Tetromino>(Tetromino::CreateRandomTetromino(StartPosition));
+		Tetrominos.push_back(std::move(NewTetromino));
+	}
+
+	CurrentTetromino = Tetrominos.begin();
+
+	TetrisBoard = std::make_unique<Board>(10, 20);
+	TetrisBoard->RegisterTetromino(*CurrentTetromino->get());
 }
 
 void Game::Run()
 {
+	GameTimer.Reset();
+
+	while (!bIsDone)
+	{
+		ProcessInput();
+		Update();
+		Render();
+	}
+}
+
+void Game::ProcessInput()
+{
+	GameInput.Update();
+	GameTimer.Tick();
+
+	bIsDone = (GameInput.GetKeyPressState(Input::EKeyType::Escape) == Input::EPressState::Released);
+	bCanMove = false;
+	CurrentStepTime += GameTimer.DeltaTime();
+
+	if (GameInput.GetKeyPressState(Input::EKeyType::Up) == Input::EPressState::Pressed)
+	{
+		bCanMove = true;
+		Movement = Tetromino::EMovement::CW;
+	}
+
+	if (GameInput.GetKeyPressState(Input::EKeyType::Down) == Input::EPressState::Pressed)
+	{
+		bCanMove = true;
+		Movement = Tetromino::EMovement::Down;
+	}
+
+	if (GameInput.GetKeyPressState(Input::EKeyType::Left) == Input::EPressState::Pressed)
+	{
+		bCanMove = true;
+		Movement = Tetromino::EMovement::Left;
+	}
+
+	if (GameInput.GetKeyPressState(Input::EKeyType::Right) == Input::EPressState::Pressed)
+	{
+		bCanMove = true;
+		Movement = Tetromino::EMovement::Right;
+	}
+
+	if (CurrentStepTime >= MaxStepTime)
+	{
+		CurrentStepTime = 0.0f;
+		bCanMove = true;
+		Movement = Tetromino::EMovement::Down;
+	}
+}
+
+void Game::Update()
+{
+	if (bCanMove)
+	{
+		TetrisBoard->UnregisterTetromino(*CurrentTetromino->get());
+		CurrentTetromino->get()->Move(Movement);
+
+		if (!TetrisBoard->RegisterTetromino(*CurrentTetromino->get()))
+		{
+			CurrentTetromino->get()->Move(Tetromino::GetCountMovement(Movement));
+			TetrisBoard->RegisterTetromino(*CurrentTetromino->get());
+
+			if (Movement == Tetromino::EMovement::Down)
+			{
+				TetrisBoard->Update();
+
+				CurrentTetromino = Tetrominos.erase(CurrentTetromino);
+				std::unique_ptr<Tetromino> NewTetromino = std::make_unique<Tetromino>(Tetromino::CreateRandomTetromino(StartPosition));
+				Tetrominos.push_back(std::move(NewTetromino));
+
+				TetrisBoard->RegisterTetromino(*CurrentTetromino->get());
+			}
+		}
+	}
+}
+
+void Game::Render()
+{
+	TetrisBoard->Draw(Math::Vec2i(0, 3));
+
+	std::list<std::unique_ptr<Tetromino>>::iterator Iter = Tetrominos.begin();
+
+	int32_t y = 3;
+	Iter++;
+	for (; Iter != Tetrominos.end(); ++Iter)
+	{
+		Iter->get()->Draw(Math::Vec2i(12, y));
+		y += 4;
+	}
 }
--- Source/Game.h
+++ Source/Game.h
@@ -4,8 +4,8 @@
 
 #include "Block.h"
 #include "Board.h"
-#include "Console.h"
 #include "Input.h"
+#include "Console.h"
 #include "Tetromino.h"
 #include "Timer.h"
 
@@ -19,6 +19,19 @@
  */
 class Game
 {
+public:
+	/**
+	 * 테트리스 게임의 상태입니다.
+	 */
+	enum class GameState : int32_t
+	{
+		Start  = 0,
+		Play   = 1,
+		Paused = 2,
+		Done   = 3
+	};
+
+
 public:
 	/**
 	 * 테트리스 게임 클래스의 생성자입니다.
@@ -74,5 +87,93 @@ public:
 
 
 private:
+	/**
+	 * 테트리스 게임 내에 입력 처리를 수행합니다.
+	 */
+	void ProcessInput();
 
+
+	/**
+	 * 테트리스 게임의 로직을 업데이트합니다.
+	 */
+	void Update();
+
+
+	/**
+	 * 테트리스 게임을 화면에 그립니다.
+	 */
+	void Render();
+
+
+private:
+	/**
+	 * 게임 종료 여부를 확인합니다.
+	 */
+	bool bIsDone = false;
+
+
+	/**
+	 * 현재 게임 상태입니다.
+	 */
+	GameState CurrentGameState = GameState::Start;
+
+
+	/**
+	 * 테트리스 게임 내에서 입력 처리를 수행합니다.
+	 */
+	Input GameInput;
+
+
+	/**
+	 * 테트리스 게임 내의 보드입니다.
+	 */
+	std::unique_ptr<Board> TetrisBoard = nullptr;
+
+
+	/**
+	 * 테트리스 게임 내의 테트로미노들입니다.
+	 */
+	std::list<std::unique_ptr<Tetromino>> Tetrominos;
+
+
+	/**
+	 * 현재 테트리스 보드 내에 배치된 테트로미노입니다.
+	 */
+	std::list<std::unique_ptr<Tetromino>>::iterator CurrentTetromino;
+
+
+	/**
+	 * 테트리스 게임의 타이머입니다.
+	 */
+	Timer GameTimer;
+
+
+	/**
+	 * 테트로미노의 시작점입니다.
+	 */
+	Math::Vec2i StartPosition;
+
+
+	/**
+	 * 테트로미노가 움직일 수 있는지를 확인합니다.
+	 */
+	bool bCanMove = false;
+
+
+	/**
+	 * 테트로미노의 움직임 방향입니다.
+	 */
+	Tetromino::EMovement Movement;
+
+
+	/**
+	 * 테트리스 게임의 최대 스텝 시간입니다.
+	 */
+	float MaxStepTime = 1.5f;
+
+
+	/**
+	 * 테트리스 게임의 현재 스텝 시간입니다.
+	 */
+	float CurrentStepTime = 0.0f;
 };
