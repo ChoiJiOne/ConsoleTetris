b89d780162716b61b6a3e72567f9297846907305
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-09-04 18:43:02 +0900

    Mathematics의 난수 생성 기능 분리
    - 필요 없는 Mathematics.cpp 파일 삭제

--- Source/Mathematics.h
+++ Source/Mathematics.h
@@ -1,60 +1,10 @@
 #pragma once
 
 #include <cstdint>
-#include <chrono>
-#include <random>
-
+#include <algorithm>
 
 namespace Math
 {
-	/**
-	 * 임의의 정수를 생성합니다.
-	 *
-	 * @param InMinValue - 생성할 난수 범위의 최솟값입니다.
-	 * @param InMaxValue - 생성할 난수 범위의 최댓값입니다.
-	 *
-	 * @return  생성된 임의의 정수를 반환합니다.
-	 */
-	template <typename T>
-	inline static int32_t GenerateRandomInt(T InMinValue, T InMaxValue)
-	{
-		if (InMinValue > InMaxValue)
-		{
-			std::swap(InMinValue, InMaxValue);
-		}
-
-		std::random_device RandomDevice;
-		std::mt19937 Generator(RandomDevice());
-		std::uniform_int_distribution<T> Distribution(InMinValue, InMaxValue);
-
-		return Distribution(Generator);
-	}
-
-
-	/**
-	 * 임의의 실수를 생성합니다.
-	 *
-	 * @param InMinValue - 생성할 난수 범위의 최솟값입니다.
-	 * @param InMaxValue - 생성할 난수 범위의 최댓값입니다.
-	 *
-	 * @return  생성된 임의의 실수를 반환합니다.
-	 */
-	template <typename T>
-	inline static float GenerateRandomFloat(T InMinValue, T InMaxValue)
-	{
-		if (InMinValue > InMaxValue)
-		{
-			std::swap(InMinValue, InMaxValue);
-		}
-
-		std::random_device RandomDevice;
-		std::mt19937 Generator(RandomDevice());
-		std::uniform_real_distribution<T> Distribution(InMinValue, InMaxValue);
-
-		return Distribution(Generator);
-	}
-
-
 	/**
 	 * 입력 받은 값을 특정 범위의 값으로 자릅니다.
 	 *
