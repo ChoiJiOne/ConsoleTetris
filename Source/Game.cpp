f2216dfbd6cf448424d0b354ba26484f2d6154e0
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2022-09-07 09:49:01 +0900

    Game 클래스 내에 TetrisMenu 부분과 겹치는 부분 삭제 및 TetrosMenu 추가
    - TODO : 버그 수정 필요 (새로운 테트로미노를 배치하면 안 되는 상황에서 배치하는 버그)

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -3,10 +3,6 @@
 
 Game::~Game()
 {
-	for (auto& GameMenu : Menus_)
-	{
-		GameMenu.second.reset();
-	}
 }
 
 void Game::Init()
@@ -15,29 +11,8 @@ void Game::Init()
 	Console::SetTitle("ConsoleTetris");
 	Console::SetCursorVisible(false);
 
-	std::vector<std::string> StartMenuElement = {
-		"■ 게임 시작",
-		"■ 게임 종료",
-	};
-	std::unique_ptr<Menu> StartMenu = std::make_unique<Menu>(StartMenuElement, Console::ETextColor::LightBlue, Console::ETextColor::White);
-	Menus_.insert({ EGameState::Start , std::move(StartMenu) });
-
-	std::vector<std::string> PausedMenuElement = {
-		"■ 게임 계속 플레이",
-		"■ 게임 재시작",
-		"■ 게임 종료",
-	};
-	std::unique_ptr<Menu> PausedMenu = std::make_unique<Menu>(PausedMenuElement, Console::ETextColor::LightBlue, Console::ETextColor::White);
-	Menus_.insert({ EGameState::Paused, std::move(PausedMenu) });
-
-	std::vector<std::string> DoneMenuElement = {
-		"■ 게임 재시작",
-		"■ 게임 종료",
-	};
-	std::unique_ptr<Menu> DoneMenu = std::make_unique<Menu>(DoneMenuElement, Console::ETextColor::LightBlue, Console::ETextColor::White);
-	Menus_.insert({ EGameState::Done, std::move(DoneMenu) });
-
 	Tetris_.Init();
+	TetrisMenu_.Init();
 }
 
 void Game::Run()
@@ -57,122 +32,79 @@ void Game::ProcessInput()
 	Input_.Update();
 	Timer_.Tick();
 
-	if (CurrentGameState_ == EGameState::Play)
+	if (bIsPlayGame)
 	{
 		Tetris_.ProcessInput(Input_);
 	}
 	else
 	{
-		Menus_[CurrentGameState_]->PrecessInput(Input_);
+		TetrisMenu_.ProcessInput(Input_);
 	}
 }
 
 void Game::Update()
 {
-	if (CurrentGameState_ == EGameState::Play)
+	if (bIsPlayGame)
 	{
 		Tetris_.Update(Timer_.DeltaTime());
 
-		if (!Tetris_.IsContinue())
+		if (Tetris_.IsPaused())
 		{
-			CurrentGameState_ = EGameState::Paused;
-			Console::Clear();
+			bIsPlayGame = false;
+			TetrisMenu_.SetMenuState(TetrisMenu::EMenuState::Paused);
+		}
+
+		if (!Tetris_.IsProcess())
+		{
+			bIsPlayGame = false;
+			TetrisMenu_.SetMenuState(TetrisMenu::EMenuState::Done);
 		}
 	}
 	else
 	{
-		if (Menus_[CurrentGameState_]->IsSwitch())
+		TetrisMenu_.Update(Timer_.DeltaTime());
+		bIsDone_ = TetrisMenu_.IsSelectQuitGame();
+
+		if (TetrisMenu_.IsSwitchMenu() && TetrisMenu_.GetMenuState() == TetrisMenu::EMenuState::Play)
 		{
-			std::string CurrentSelectElement = Menus_[CurrentGameState_]->GetCurrentSelectElement();
-			Console::Clear();
+			bIsPlayGame = true;
+			Tetris_.Continue();
 
-			static std::pair<std::string, EGameState> MenuElements[] = {
-				{ "■ 게임 시작",       EGameState::Play },
-				{ "■ 게임 종료",       EGameState::Exit},
-				{ "■ 게임 계속 플레이", EGameState::Play},
-				{ "■ 게임 재시작",     EGameState::Play},
-			};
+			float CurrentStepTime = 1.5f - (static_cast<float>(TetrisMenu_.GetSelectLevel() - 1) * 0.2f);
+			Tetris_.SetStepTime(CurrentStepTime);
+		}
 
-			for (const auto& MenuElement : MenuElements)
-			{
-				if (!CurrentSelectElement.compare(MenuElement.first))
-				{
-					CurrentGameState_ = MenuElement.second;
-
-					if (!CurrentSelectElement.compare("■ 게임 종료"))
-					{
-						bIsDone_ = true;
-					}
-
-					if (!CurrentSelectElement.compare("■ 게임 계속 플레이"))
-					{
-						Tetris_.SetContinue(true);
-					}
-
-					if (!CurrentSelectElement.compare("■ 게임 재시작"))
-					{
-						Tetris_.Reset();
-						Timer_.Reset();
-					}
-
-					break;
-				}
-			}
+		if (TetrisMenu_.IsSwitchMenu() && TetrisMenu_.GetMenuState() == TetrisMenu::EMenuState::Start)
+		{
+			Tetris_.Reset();
+			Timer_.Reset();
 		}
 	}
 }
 
 void Game::Draw()
 {
-	if (CurrentGameState_ == EGameState::Play)
+	if (bIsPlayGame)
 	{
 		Vec2i BoardPosition = Vec2i(9, 2);
 		Tetris_.Draw(BoardPosition);
 
 		Vec2i PlayTimePosition = Vec2i(0, 5);
 		int32_t PlayTime = static_cast<int32_t>(Timer_.TotalTime());
-		Console::DrawText(PlayTimePosition.x, PlayTimePosition.y, Text::Format(" 플레이 시간:%ds", PlayTime), Console::ETextColor::LightGreen);
+		Console::DrawText(PlayTimePosition.x, PlayTimePosition.y, "플레이 시간", Console::ETextColor::LightGreen);
+		Console::DrawText(PlayTimePosition.x, PlayTimePosition.y + 1, Text::Format("%ds", PlayTime), Console::ETextColor::LightGreen);
 
 		Vec2i UserLevelPosition = Vec2i(0, 10);
-		Console::DrawText(UserLevelPosition.x, UserLevelPosition.y, Text::Format(" 레벨:%d", 1), Console::ETextColor::LightGreen);
+		Console::DrawText(UserLevelPosition.x, UserLevelPosition.y, "레벨", Console::ETextColor::LightGreen);
+		Console::DrawText(UserLevelPosition.x, UserLevelPosition.y + 1, Text::Format("%d", 1), Console::ETextColor::LightGreen);
 
 		Vec2i RemoveLinePosition = Vec2i(0, 15);
-		Console::DrawText(RemoveLinePosition.x, RemoveLinePosition.y, Text::Format(" 삭제한 라인:%d", Tetris_.GetRemoveLine()), Console::ETextColor::LightGreen);
+		Console::DrawText(RemoveLinePosition.x, RemoveLinePosition.y, Text::Format(" 삭제한 라인", Tetris_.GetRemoveLine()), Console::ETextColor::LightGreen);
+		Console::DrawText(RemoveLinePosition.x, RemoveLinePosition.y + 1, Text::Format("%d", Tetris_.GetRemoveLine()), Console::ETextColor::LightGreen);
 	}
 	else
 	{
-		Vec2i TitlePosition = Vec2i(0, 0);
-		DrawConsoleTetrisTitle(TitlePosition, Console::ETextColor::LightAqua);
-
-		if (CurrentGameState_ != EGameState::Exit)
-		{
-			Vec2i MenuPosition = Vec2i(7, 13);
-			Menus_[CurrentGameState_]->Draw(MenuPosition);
-		}
-	}
-}
-
-void Game::DrawConsoleTetrisTitle(const Vec2i& InPosition, const Console::ETextColor& InColor)
-{
-	static std::string Title[] = {
-		"   _____                      _      ",
-		"  / ____|                    | |     ",
-		" | |     ___  _ __  ___  ___ | | ___ ",
-		" | |    / _ \\| \'_ \\/ __|/ _ \\| |/ _ \\",
-		" | |___| (_) | | | \\__ \\ (_) | |  __/",
-		"  \\_____\\___/|_| |_|___/\\___/|_|\\___|",
-		"  _______ ______ _______ _____  _____  _____ ",
-		" |__   __|  ____|__   __|  __ \\|_   _|/ ____|",
-		"    | |  | |__     | |  | |__) | | | | (___  ",
-		"    | |  |  __|    | |  |  _  /  | |  \\___ \\ ",
-		"    | |  | |____   | |  | | \\ \\ _| |_ ____) |",
-		"    |_|  |______|  |_|  |_|  \\_\\_____|_____/ ",
-	};
-
-	Vec2i Position = InPosition;
-	for (const auto& TitleLine : Title)
-	{
-		Console::DrawText(Position.x, Position.y, TitleLine, InColor);
-		Position.y += 1;
+		Vec2i MenuPosition = Vec2i(0, 0);
+		TetrisMenu_.Draw(MenuPosition);
 	}
 }
--- Source/Game.h
+++ Source/Game.h
@@ -3,8 +3,8 @@
 #define _CRTDBG_MAP_ALLOC
 
 #include "Tetris.h"
+#include "TetrisMenu.h"
 #include "Console.h"
-#include "Menu.h"
 
 #include <stdexcept>
 #include <memory>
@@ -16,20 +16,6 @@
  */
 class Game
 {
-public:
-	/**
-	 * 게임의 상태입니다.
-	 */
-	enum class EGameState : int32_t
-	{
-		Start  = 0,
-		Play   = 1,
-		Paused = 2,
-		Done   = 3,
-		Exit   = 4,
-	};
-
-
 public:
 	/**
 	 * 게임 클래스의 생성자입니다.
@@ -105,37 +91,21 @@ private:
 
 private:
 	/**
-	 * 테트리스 게임의 타이틀 화면을 그립니다.
-	 * 
-	 * @param InPosition - 콘솔 화면 상의 타이틀 왼쪽 상단 좌표입니다. 
-	 * @param InColor - 타이틀 화면의 색상입니다.
-	 */
-	void DrawConsoleTetrisTitle(const Vec2i& InPosition, const Console::ETextColor& InColor);
-
-
-private:
-	/**
 	 * 게임 종료 여부를 확인합니다.
 	 */
 	bool bIsDone_ = false;
 
 
 	/**
-	 * 현재 게임 상태입니다.
+	 * 게임을 플레이 하는지 확인합니다.
 	 */
-	EGameState CurrentGameState_ = EGameState::Start;
+	bool bIsPlayGame = false;
 
 
 	/**
 	 * 게임 내에서 입력 처리를 수행합니다.
 	 */
 	Input Input_;
-
-
-	/**
-	 * 게임의 메뉴입니다.
-	 */
-	std::unordered_map<EGameState, std::unique_ptr<Menu>> Menus_;
 
 
 	/**
@@ -148,4 +118,10 @@ private:
 	 * 테트리스 게임입니다.
 	 */
 	Tetris Tetris_;
+
+
+	/**
+	 * 테트리스 게임의 메뉴입니다.
+	 */
+	TetrisMenu TetrisMenu_;
 };
