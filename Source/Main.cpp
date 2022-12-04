bca26c03a4d3b8ea4995f0bf36cc3fba9a450a05
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2022-12-04 09:55:59 +0900

    간단한 방향키 입력 처리 테스트 코드

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -11,6 +11,7 @@ int main(int argc, char* argv[])
     timeout(0);
 
     bool bIsDone = false;
+
     while(!bIsDone)
     {
         int32_t Command = getch();
@@ -21,7 +22,29 @@ int main(int argc, char* argv[])
         case 'q':
             bIsDone = true;
             break;
+
+        case KEY_LEFT:
+            move(0, 0);
+            printw("PRESS LEFT");
+            break;
+
+        case KEY_RIGHT:
+            move(1, 0);
+            printw("PRESS RIGHT");
+            break;
+
+        case KEY_UP:
+            move(2, 0);
+            printw("PRESS UP");
+            break;
+
+        case KEY_DOWN:
+            move(3, 0);
+            printw("PRESS DOWN");
+            break;
         }
+
+        refresh();
     }
 
     curs_set(1);
