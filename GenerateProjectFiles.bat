611a3c9175bee0720516834a8f1debdc5b877ae0
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-12-11 18:34:24 +0900

    덤프 파일을 저장하는 폴더를 생성하도록 변경

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -2,12 +2,16 @@
 
 echo Generate Project Files ...
 
-if exist ConsoleTetris (
-	echo Remove Legacy Folder
-	rmdir /s /q ConsoleTetris
+if not exist Dump (
+    echo don't exist dump directory...
+    mkdir Dump
 )
 
-mkdir ConsoleTetris
+if exist ConsoleTetris (
+	echo already exist ConsoleTetris...
+)
+
+echo run premake5.exe...
 Bin\\premake5.exe vs2019
 start ConsoleTetris\\ConsoleTetris.sln
 PAUSE
