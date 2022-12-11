2e634809190c97478773af7b1ea2db1b6014e5c7
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-12-11 18:27:32 +0900

    커맨드 라인을 파싱하고 관리하는 기능 추가

---
+++ Source/CommandLineManager.cpp
@@ -0,0 +1,31 @@
+#include "CommandLineManager.h"
+#include "Text.hpp"
+#include "Macro.h"
+
+#include <Windows.h>
+
+CommandLineManager::CommandLineManager()
+{
+	CommandLine_ = GetCommandLineA();
+	Arguments_ = Text::Split(CommandLine_, " ");
+
+	for (const auto& Argument : Arguments_)
+	{
+		if (Argument.find("=") != std::string::npos)
+		{
+			std::vector<std::string> Tokens = Text::Split(Argument, "=");
+			Options_.insert({ Tokens.front(), Tokens.back() });
+		}
+	}
+}
+
+bool CommandLineManager::HaveOption(const std::string& InOption)
+{
+	return Options_.find(InOption) != Options_.end();
+}
+
+std::string CommandLineManager::GetValue(const std::string& InOption)
+{
+	CHECK(HaveOption(InOption), "can't find command line option");
+	return Options_.at(InOption);
+}
