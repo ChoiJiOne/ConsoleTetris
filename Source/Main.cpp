f6917dde6043c1f7aa254f144d6df4ff51741ac2
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-12-11 18:10:31 +0900

    ConsoleManager 구현 테스트

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "InputManager.h"
+#include "ConsoleManager.h"
 #include "Vector.hpp"
 
 int main(void)
@@ -17,21 +18,29 @@ int main(void)
 
 		if (InputManager::Get().GetKeyPressState(EKeyCode::UP) == EPressState::PRESSED)
 		{
+			ConsoleManager::Get().MoveCursor(Vec2i(10, 0));
+			ConsoleManager::Get().SetTextColor(EColor::AQUA);
 			std::cout << "PRESS UP\n";
 		}
 
 		if (InputManager::Get().GetKeyPressState(EKeyCode::DOWN) == EPressState::PRESSED)
 		{
+			ConsoleManager::Get().MoveCursor(Vec2i(10, 20));
+			ConsoleManager::Get().SetTextColor(EColor::WHITE);
 			std::cout << "PRESS DOWN\n";
 		}
 
 		if (InputManager::Get().GetKeyPressState(EKeyCode::LEFT) == EPressState::PRESSED)
 		{
+			ConsoleManager::Get().MoveCursor(Vec2i(0, 10));
+			ConsoleManager::Get().SetTextColor(EColor::GRAY);
 			std::cout << "PRESS LEFT\n";
 		}
 
 		if (InputManager::Get().GetKeyPressState(EKeyCode::RIGHT) == EPressState::PRESSED)
 		{
+			ConsoleManager::Get().MoveCursor(Vec2i(20, 10));
+			ConsoleManager::Get().SetTextColor(EColor::GREEN);
 			std::cout << "PRESS RIGHT\n";
 		}
 	}
