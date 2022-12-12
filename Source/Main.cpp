fa0c7c314c6b082db5709b7bde789704c0c791c3
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2022-12-12 17:39:21 +0900

    main 함수에 구현된 부분을 Tetris class의 Run 메서드로 이동

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -5,40 +5,90 @@
 #include "ConsoleManager.h"
 #include "Vector.hpp"
 
-int main(void)
+
+/**
+ * 테트리스 게임입니다.
+ */
+class Tetris
 {
-	SetUnhandledExceptionFilter(UnhandledExceptionHandler);
+public:
+	/**
+	 * 테트리스 게임의 생성자입니다.
+	 * 이때, 생성자에서는 어떤 초기화도 수행하지 않습니다.
+	 */
+	Tetris() = default;
 
-	bool bIsDone = false;
-	while (!bIsDone)
+
+	/**
+	 * 테트리스 게임의 가상 소멸자입니다.
+	 */
+	virtual ~Tetris()
 	{
-		InputManager::Get().Tick();
+	}
 
-		if (InputManager::Get().GetKeyPressState(EKeyCode::ESCAPE) == EPressState::PRESSED)
-		{
-			bIsDone = true;
-		}
 
-		if (InputManager::Get().GetKeyPressState(EKeyCode::UP) == EPressState::PRESSED)
-		{
-			ConsoleManager::Get().RenderText(Vec2i(10, 0), "PRESS UP", EColor::AQUA);
-		}
+	/**
+	 * 테트리스 게임을 초기화합니다.
+	 * 
+	 * @throws 테트리스 게임 초기화에 실패하면, C++ 표준 예외를 던집니다.
+	 */
+	void Init()
+	{
+		SetUnhandledExceptionFilter(UnhandledExceptionHandler);
+	}
 
-		if (InputManager::Get().GetKeyPressState(EKeyCode::DOWN) == EPressState::PRESSED)
-		{
-			ConsoleManager::Get().RenderText(Vec2i(10, 20), "PRESS DOWN", EColor::WHITE);
-		}
 
-		if (InputManager::Get().GetKeyPressState(EKeyCode::LEFT) == EPressState::PRESSED)
+	/**
+	 * 테트리스 게임을 실행합니다.
+	 * 
+	 * @throws 테트리스 게임 실행에 실패하면, C++ 표준 예외를 던집니다.
+	 */
+	void Run()
+	{
+		while (!bIsDone_)
 		{
-			ConsoleManager::Get().RenderText(Vec2i(0, 10), "PRESS LEFT", EColor::GRAY);
-		}
+			InputManager::Get().Tick();
 
-		if (InputManager::Get().GetKeyPressState(EKeyCode::RIGHT) == EPressState::PRESSED)
-		{
-			ConsoleManager::Get().RenderText(Vec2i(20, 10), "PRESS RIGHT", EColor::GREEN);
+			if (InputManager::Get().GetKeyPressState(EKeyCode::ESCAPE) == EPressState::PRESSED)
+			{
+				bIsDone_ = true;
+			}
+
+			if (InputManager::Get().GetKeyPressState(EKeyCode::UP) == EPressState::PRESSED)
+			{
+				ConsoleManager::Get().RenderText(Vec2i(10, 0), "PRESS UP", EColor::AQUA);
+			}
+
+			if (InputManager::Get().GetKeyPressState(EKeyCode::DOWN) == EPressState::PRESSED)
+			{
+				ConsoleManager::Get().RenderText(Vec2i(10, 20), "PRESS DOWN", EColor::WHITE);
+			}
+
+			if (InputManager::Get().GetKeyPressState(EKeyCode::LEFT) == EPressState::PRESSED)
+			{
+				ConsoleManager::Get().RenderText(Vec2i(0, 10), "PRESS LEFT", EColor::GRAY);
+			}
+
+			if (InputManager::Get().GetKeyPressState(EKeyCode::RIGHT) == EPressState::PRESSED)
+			{
+				ConsoleManager::Get().RenderText(Vec2i(20, 10), "PRESS RIGHT", EColor::GREEN);
+			}
 		}
 	}
+
+
+private:
+	/**
+	 * 게임 종료 여부를 확인합니다.
+	 */
+	bool bIsDone_ = false;
+};
+
+int main(void)
+{
+	auto Game = std::make_unique<Tetris>();
+	Game->Init();
+	Game->Run();
 
 	return 0;
 }
