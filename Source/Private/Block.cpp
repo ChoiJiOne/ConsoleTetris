d6d5387d09c251a517043e5bfa95df8bd182e757
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2022-12-13 11:16:40 +0900

    블럭의 콘솔 화면에 그리는 기능 추가

---
+++ Source/Private/Block.cpp
@@ -0,0 +1,11 @@
+#include <Block.h>
+#include <ConsoleManager.h>
+
+void Block::Render(const Vec2i& InPosition)
+{
+	Vec2i ConsolePosition = InPosition + Position_;
+	EColor Color = static_cast<EColor>(Type_);
+	std::string Text = (State_ == EState::EMPTY) ? "¡¤" : "¡á";
+	
+	ConsoleManager::Get().RenderText(ConsolePosition, Text, Color);
+}
