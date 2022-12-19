41b4c95e132d10770ac8b2ff787a91cca04feb67
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-12-19 20:40:23 +0900

    게임 오브젝트의 컴포넌트 부분 삭제

--- Source/Private/GameObject.cpp
+++ Source/Private/GameObject.cpp
@@ -1,10 +1,3 @@
 #include <GameObject.h>
-#include <Component.h>
 
-GameObject::~GameObject()
-{
-	for (auto& OwnComponent : Components_)
-	{
-		OwnComponent.second.reset();
-	}
-}
+GameObject::~GameObject() {}
--- Source/Public/GameObject.h
+++ Source/Public/GameObject.h
@@ -2,11 +2,6 @@
 
 #include <Macro.h>
 
-#include <unordered_map>
-#include <memory>
-
-class Component;
-
 
 /**
  * 게임 오브젝트 클래스입니다.
@@ -45,72 +40,4 @@ public:
 	 * 게임 오브젝트를 화면에 그립니다.
 	 */
 	virtual void Render() = 0;
-
-
-	/**
-	 * 게임 오브젝트에 컴포넌트를 추가합니다.
-	 * 이때, 키 값에 대응하는 컴포넌트가 존재한다면 아무런 동작도 수행하지 않습니다.
-	 *
-	 * @param InKey - 추가할 컴포넌트의 키값입니다.
-	 * @param InArgs - 컴포넌트에 필요한 가변 인자입니다.
-	 */
-	template<typename T, typename... Types>
-	void AddComponent(const std::size_t& InKey, Types... InArgs)
-	{
-		if (HaveComponent(InKey)) return;
-
-		std::unique_ptr<T> NewComponent = std::make_unique<T>(this, InArgs...);
-		Components_.insert({ InKey, std::move(NewComponent) });
-	}
-
-
-	/**
-	 * 게임 오브젝트가 소유하고 있는 컴포넌트를 얻습니다.
-	 *
-	 * @param InKey - 게임 오브젝트가 소유하고 있는 컴포넌트의 키 값입니다.
-	 *
-	 * @return 키 값에 대응하는 컴포넌트가 존재하면 주소 값을 반환, 그렇지 않으면 nullptr를 반환합니다.
-	 */
-	template<typename T>
-	T* GetComponent(const std::size_t& InKey)
-	{
-		if (!HaveComponent(InKey)) return nullptr;
-
-		return reinterpret_cast<T*>(Components_.at(InKey).get());
-	}
-
-
-	/**
-	 * 게임 오브젝트가 소유하고 있는 컴포넌트를 삭제합니다.
-	 * 이때, 키 값에 대응하는 컴포넌트가 존재하지 않는다면, 아무런 동작도 하지 않습니다.
-	 *
-	 * @param InKey - 게임 오브젝트가 소유하고 있는 컴포넌트의 키 값입니다.
-	 */
-	template<typename T>
-	void RemoveComponent(const std::size_t& InKey)
-	{
-		if (!HaveComponent(InKey)) return;
-
-		Components_.erase(InKey);
-	}
-
-
-	/**
-	 * 키 값에 대응하는 컴포넌트가 존재하는지 검사합니다.
-	 *
-	 * @param InKey - 검사를 수행할 키 값입니다.
-	 *
-	 * @return 키 값에 대응하는 컴포넌트가 존재하면 true, 그렇지 않으면 false를 반환합니다.
-	 */
-	bool HaveComponent(const std::size_t& InKey)
-	{
-		return Components_.find(InKey) != Components_.end();
-	}
-
-
-private:
-	/**
-	 * 게임 오브젝트가 소유하고 있는 컴포넌트입니다.
-	 */
-	std::unordered_map<std::size_t, std::unique_ptr<Component>> Components_;
 };
