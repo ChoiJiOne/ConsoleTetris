72bd381f5661f268314c3da94c4322e64834a353
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2022-12-27 12:19:59 +0900

    시작 씬에서 타이틀을 렌더링 하는 기능 추가

--- Source/Private/StartSceneNode.cpp
+++ Source/Private/StartSceneNode.cpp
@@ -1,4 +1,5 @@
 #include <StartSceneNode.h>
+#include <ConsoleManager.h>
 
 StartSceneNode::StartSceneNode()
 	: SceneNode("StartScene")
@@ -15,4 +16,25 @@ void StartSceneNode::Update(float InDeltaSeconds)
 
 void StartSceneNode::Render()
 {
+	static std::string Title[] = {
+		"   _____                      _      ",
+		"  / ____|                    | |     ",
+		" | |     ___  _ __  ___  ___ | | ___ ",
+		" | |    / _ \\| \'_ \\/ __|/ _ \\| |/ _ \\",
+		" | |___| (_) | | | \\__ \\ (_) | |  __/",
+		"  \\_____\\___/|_| |_|___/\\___/|_|\\___|",
+		"  _______ ______ _______ _____  _____  _____ ",
+		" |__   __|  ____|__   __|  __ \\|_   _|/ ____|",
+		"    | |  | |__     | |  | |__) | | | | (___  ",
+		"    | |  |  __|    | |  |  _  /  | |  \\___ \\ ",
+		"    | |  | |____   | |  | | \\ \\ _| |_ ____) |",
+		"    |_|  |______|  |_|  |_|  \\_\\_____|_____/ ",
+	};
+
+	Vec2i ConsolePosition = Vec2i(5, 5);
+	for (const auto& TitleLine : Title)
+	{
+		ConsoleManager::Get().RenderText(ConsolePosition, TitleLine, EColor::AQUA);
+		ConsolePosition.y += 1;
+	}
 }
